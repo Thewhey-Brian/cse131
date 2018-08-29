6b2fe11d5b7abd79aadf509c19f8573668ad14d7
serendipity <serendipity@guoxiangerdembp.dhcp.wustl.edu>
2018-08-28 21:55:32 -0500

    changes of exercise1

--- exercises/exercises1/Change.java
+++ exercises/exercises1/Change.java
@@ -8,14 +8,20 @@ public class Change {
 		//
 		// Below, prompt the user to enter a number of pennies
 		//
-		
-		
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int pennies = ap.nextInt("Staring number of pennies? ");
+		int dollars = pennies/100;
+		int quarters = pennies%100/25;
+		int dimes = (pennies%100-quarters*25)/10;
+		int nickels = (pennies%100-quarters*25)%10/5;
+		int pennies0 = (pennies%100-quarters*25)%10-nickels;
 		//
 		// Then, compute and print out how many 
 		//    dollars, quarters, dimes, nickels, and pennies
 		// should be given in exchange for those pennies, so as to
 		// minimize the number of coins (see the videos)
 		//
+		System.out.println("For "+pennies+" pennies:\n"+dollars+" dollars\n"+quarters+" quarters\n"+dimes+" dimes\n"+nickels+" nickels\n"+pennies0+" pennies\n");
 	}
 
 }
