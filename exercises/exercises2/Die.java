af49fe1342e3a65a1c944d3894b28d1a2daa1280
serendipity <serendipity@guoxiangerdembp.dhcp.wustl.edu>
2018-09-02 17:01:41 -0500

    Exercise 2.6

--- exercises/exercises2/Die.java
+++ exercises/exercises2/Die.java
@@ -3,6 +3,39 @@ package exercises2;
 public class Die {
 
 	public static void main(String[] args) {
+		
+		double die = Math.random();
+		if (die>0.16667)
+		{
+			if(die>0.33333)
+			{
+				if(die>0.5)
+				{
+					if(die>0.66666)
+					{
+						if(die>0.83333)
+						{
+							System.out.println("1");
+						}
+						else {
+							System.out.println("2");
+						}
+					}
+					else {
+						System.out.println("3");
+					}
+				}
+				else {
+					System.out.println("4");
+				}
+			}
+			else {
+				System.out.println("5");
+			}
+		}
+		else {
+			System.out.println("6");
+		}
 
 		//
 		// Use the random number generator
