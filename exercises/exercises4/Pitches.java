07257906833ec57846819417bc6bcd2a3a87a973
xinyuguo <xinyuguo@xinyus-mbp.dhcp.wustl.edu>
2018-09-16 20:47:54 -0500

    Exercise 4.5

--- exercises/exercises4/Pitches.java
+++ exercises/exercises4/Pitches.java
@@ -8,6 +8,16 @@ public class Pitches {
 		// encoding has 0 as concert A
 		//    1 would be Bb, just above A
 		//   -1 would be Ab, just below A
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int pitch = ap.nextInt("What pitch?");
+		int p7 = pitch+7;
+		double frequency = 440.0*Math.pow(2, pitch/12.0);
+		double f7 = 440.0*Math.pow(2, p7/12.0);
+		System.out.println("Pitch "+pitch+"gets the frequency "+frequency);
+		System.out.println("Pitch7 "+p7+"gets the frequency7 "+f7);
+		double ratio = f7/frequency;
+		System.out.println("The ratio is "+ratio);
+		
 
 
 	}
