b50a85a3cb3137deb3b1d24054bc4c41a36aa2d0
xinyuguo <xinyuguo@xinyus-mbp.dhcp.wustl.edu>
2018-10-03 18:10:14 -0500

    Exercise 6.3

--- exercises/exercises6/FactorialNoBaseCase.java
+++ exercises/exercises6/FactorialNoBaseCase.java
@@ -3,7 +3,14 @@ package exercises6;
 public class FactorialNoBaseCase {
 	
 	public static int factorial(int n) {
-		return n * factorial(n-1);
+		if(n!=0) {
+			return n * factorial(n-1);
+		}
+		else {
+			return 1;
+		}
+		// don't have the base cases
+		
 	}
 
 	public static void main(String[] args) {
