01a90b8f200c5873224e02af62c705791fc894f6
xinyuguo <xinyuguo@xinyus-mbp.dhcp.wustl.edu>
2018-10-02 21:16:34 -0500

    Exercise 5.1

--- exercises/exercises5/Recipe.java
+++ exercises/exercises5/Recipe.java
@@ -9,7 +9,18 @@ public class Recipe {
 		// Below, use comments or actual Java code
 		//  to type in the abstractions
 		//  you found in the recipe story
-		// 
+		// heat(thing, volume, degree, time)
+		// divide(thing, pieces)
+		//heat(water, 200cc, 100,)
+		//mixWithSpoon(5 grams of yeast, water, slow)
+		//heat(butter, , 60, 30min)
+		//mixWithSpoon(butter, vanilla, slow)
+		//heat(, , 20, 5min)
+		//mixWithSpoon(butter, sugar, yeast, flour)
+		//heat(mixture, , 110, 45min)
+		//divide(mixture, 10)
+		//heat(divided mixture, , 350, 45min)
+		//
 
 		//
 		// Use those abstractions in comments or code to 
