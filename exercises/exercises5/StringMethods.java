7c35f89e8774d4b20b2a8c1f65259e7006b37199
xinyuguo <xinyuguo@xinyus-mbp.dhcp.wustl.edu>
2018-10-02 22:25:05 -0500

    Exercise 5.2

--- exercises/exercises5/StringMethods.java
+++ exercises/exercises5/StringMethods.java
@@ -1,5 +1,7 @@
 package exercises5;
 
+import sun.security.util.Length;
+
 public class StringMethods {
 	
 	//
@@ -13,7 +15,30 @@ public class StringMethods {
 	//
 	
 	public static String selfConcat(String s) {
-		return s + s;
+		//return s + s;
+		return copies(s, 2);
+	}
+	
+	public static String copies(String s, int n) {
+		//String s0 = "";
+		//for (int i=0; i<n; i++) {
+		//	s0 = s0 + s;
+		//}
+		//return s0;
+		String[] array = new String[n];
+		for (int i=0; i<n; i++) {
+			array[i] = s;
+		}
+		return join(array, "");
+		
+	}
+	
+	public static String join(String[] array, String joiner) {
+		String result = array[0];
+		for (int i=1; i<array.length; ++i) {
+			result = result + joiner + array[i];
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
@@ -22,6 +47,15 @@ public class StringMethods {
 		// Testing the methods
 		//
 		System.out.println(selfConcat("echo"));
+		System.out.println(copies("Hello", 6));
+		System.out.println(copies("echo", 2));
+		String s = copies("wow!", 8);
+		String t = "I love you!";
+		System.out.println(s);
+		String[] w = s.split("!");
+		String[] k = t.split(" ");
+		System.out.println(join(w, "@_@"));
+		System.out.println(join(k, "!!! "));
 	}
 
 }
