bd0cdf8258b6ece3ba9cb90eff9d382d361fcd5c
xinyuguo <xinyuguo@xinyus-mbp.dhcp.wustl.edu>
2018-10-04 16:51:12 -0500

    Studio 5

--- studios/studio5/MethodsTest.java
+++ studios/studio5/MethodsTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MethodsTest {
-
+    /**
+     * this test the sum by using for loop
+     */
 	@Test
 	public void testSum() {
 		//
@@ -23,33 +25,50 @@ public class MethodsTest {
 		}
 	}
 	
+	/**
+	 * this test the mpy by choosing two value
+	 */
 	@Test
 	public void thisTestShouldFail() {
 		assertEquals(0, Methods.mpy(0,3));
 		assertEquals(10, Methods.mpy(2, 5));
 	}
-	
+	/**
+	 * this test the pig by if the specific string
+	 */
 	@Test
 	public void testPig() {
 		assertEquals("omputercay", Methods.pig("computer"));
 		assertEquals("unfay", Methods.pig("fun"));
 	}
 	
+	/**
+	 * this test the arraysum by adding a specific array
+	 */
 	@Test
 	public void testArraySum() {
 		assertEquals(6.0, Methods.sumArray(new double[] { 1, 2, 3}), 0.1);
 	}
 	
+	/**
+	 * this test the avg3 by average three specific numbers
+	 */
 	@Test
 	public void testAvg3() {
 		assertEquals(2.0, Methods.avg3(1,2,3), 0.1);		
 	}
 	
+	/**
+	 * this test the average by applying it to two specific numbers
+	 */
 	@Test
 	public void testAverage() {
 		assertEquals(0.5, Methods.average(new double[] { 0, 1 }), 0.1);
 	}
 	
+	/**
+	 * this test the absolute by testing -10
+	 */
 	@Test
 	public void absolute() {
 		assertEquals(10, Methods.absolute(-10));
