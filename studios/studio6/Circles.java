3d1b6322fec9b423b161e0255075898e6389f2d9
xinyuguo <xinyuguo@xinyus-mbp.dhcp.wustl.edu>
2018-10-18 16:36:06 -0500

    studio6 circle

--- studios/studio6/Circles.java
+++ studios/studio6/Circles.java
@@ -5,12 +5,25 @@ import sedgewick.StdDraw;
 
 public class Circles {
 	private static void recursiveCircles(double xCenter, double yCenter, double radius) {
-		throw new NotYetImplementedException("delete this line of code and implement this method.");
+		 if(radius<0.005) {
+			 return;
+		 }
+		 
+		 StdDraw.circle(xCenter, yCenter, radius);
+		 
+		 //StdDraw.pause(500);
+		 
+		 recursiveCircles(xCenter-radius, yCenter, (2-Math.sqrt(2))*radius/2);
+		 recursiveCircles(xCenter, yCenter-radius, (2-Math.sqrt(2))*radius/2);
+		 recursiveCircles(xCenter+radius, yCenter, (2-Math.sqrt(2))*radius/2);
+		 recursiveCircles(xCenter, yCenter+radius, (2-Math.sqrt(2))*radius/2);
+		 
 	}
 
 	public static void main(String[] args) {
 		StdDraw.setXscale(-1, +1);
 		StdDraw.setYscale(-1, +1);
 		recursiveCircles(0, 0, 0.5);
+		StdDraw.show(10);
 	}
 }
