89a8f3f81e449c96ae00fd7c967dd9bfbf8c7932
xinyuguo <xinyuguo@xinyus-mbp.dhcp.wustl.edu>
2018-10-28 22:41:34 -0500

    Exercise 8.1

---
+++ exercises/exercises8/ThinkAboutList.java
@@ -0,0 +1,17 @@
+package exercises8;
+
+public class ThinkAboutList {
+	//1, 
+	//List<Color> : The color of the flowers on the way to school
+	// (can be duplicate)
+	//List<Account> : The bank account that a man has during his life time
+	// (in the order of time)
+	//2, 
+	//List<Account>
+	//3, 
+	//After an exam, a teacher will grade the exam paper and he can use the List<Boolean>
+	//to record the outcome.
+	//4, 
+	//addition, subtraction, multiplication
+	
+}
