e77378aecc0ba15a2f4a7e4439234ef2b32745f0
xinyuguo <xinyuguo@xinyus-mbp.dhcp.wustl.edu>
2018-12-04 13:34:47 -0600

    extension

--- extensions/matrix/Gaussian.java
+++ extensions/matrix/Gaussian.java
@@ -16,7 +16,28 @@ public class Gaussian {
 	 * @return
 	 */
 	public Matrix getSolution(){
-		return null;  // FIXME
+		for(int i=0; i<coeff.getNumRows()-1; ++i) {
+			for(int j=i+1; j<coeff.getNumRows(); ++j) {
+				coeff.scaleRow(i, -1/coeff.getValue(i, i)*coeff.getValue(j, 0));
+				constants.scaleRow(i, -1/coeff.getValue(i, i)*coeff.getValue(j, 0));
+				coeff.addRows(i, j);
+				constants.addRows(i, j);
+				coeff.scaleRow(i, 1/coeff.getValue(i, i));
+				constants.scaleRow(i, 1/coeff.getValue(i, i));
+			}
+		}
+		coeff.scaleRow(coeff.getNumRows()-1, 1/coeff.getValue(coeff.getNumRows()-1, coeff.getNumRows()-1));
+		constants.scaleRow(coeff.getNumRows()-1, 1/constants.getValue(coeff.getNumRows()-1, coeff.getNumRows()-1));
+		for(int t=coeff.getNumRows()-1; t>0; --t) {
+			for(int k=t-1; k>=0; --k) {
+				coeff.scaleRow(t, -1/coeff.getValue(t, t)*coeff.getValue(k, t));
+				constants.scaleRow(t, -1/coeff.getValue(t, t)*coeff.getValue(k, t));
+				coeff.addRows(t, k);
+				coeff.addRows(t, k);
+			}
+		}
+		return constants;
+		
 	}
 	
 	public String toString(){
